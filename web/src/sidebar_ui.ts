f1d1dcc60ed3efea0a0d161a1e22de95c5c496c7
Aditya Kasaudhan <akasaudhan02@gmail.com>
2025-09-18 20:17:17 +0530

    navigation_views: Add frontend of navigation views.
    Fixes: #32077.

--- web/src/sidebar_ui.ts
+++ web/src/sidebar_ui.ts
@@ -306,11 +306,11 @@ export function update_expanded_views_for_search(search_term: string): void {
     }
 
     let any_view_visible = false;
-    const expanded_views = left_sidebar_navigation_area.get_built_in_views();
     const show_all_views = util.prefix_match({
         value: LEFT_SIDEBAR_NAVIGATION_AREA_TITLE,
         search_term,
     });
+    const expanded_views = left_sidebar_navigation_area.get_built_in_pinned_views();
     for (const view of expanded_views) {
         let show_view =
             show_all_views ||
@@ -335,17 +335,24 @@ export function update_expanded_views_for_search(search_term: string): void {
 }
 
 export function initialize_left_sidebar(): void {
-    const primary_condensed_views =
-        left_sidebar_navigation_area.get_built_in_primary_condensed_views();
-    const expanded_views = left_sidebar_navigation_area.get_built_in_views();
+    const pinned_views = left_sidebar_navigation_area.get_built_in_pinned_views();
+    const unpinned_views = left_sidebar_navigation_area.get_built_in_unpinned_views();
+    const primary_condensed_views = left_sidebar_navigation_area.get_primary_condensed_views();
+    const has_unpinned_views = unpinned_views.length > 0;
+    const is_condensed = left_sidebar_navigation_area.is_condensed();
+    const should_hide_menu = !is_condensed && !has_unpinned_views;
+    const show_empty_state = !is_condensed && pinned_views.length === 0;
 
     const rendered_sidebar = render_left_sidebar({
         is_guest: current_user.is_guest,
+        development_environment: page_params.development_environment,
         is_spectator: page_params.is_spectator,
         primary_condensed_views,
-        expanded_views,
         LEFT_SIDEBAR_NAVIGATION_AREA_TITLE,
         LEFT_SIDEBAR_DIRECT_MESSAGES_TITLE: pm_list.LEFT_SIDEBAR_DIRECT_MESSAGES_TITLE,
+        expanded_views: pinned_views,
+        should_hide_menu,
+        show_empty_state,
     });
 
     $("#left-sidebar-container").html(rendered_sidebar);
